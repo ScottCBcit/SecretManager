75b0cc67cda2500abced0b4e1432942da393191f
Scott Croin <scroin@my.bcit.ca>
2023-01-23 11:15:23 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecretManager.Models;
+using System.Diagnostics;
+
+namespace SecretManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            var adminUserName = _config["adminLogin:Username"];
+            var adminPassword = _config["AdminLogin:Password"];
+            var connectionString = _config["ConnectionStrings:DefaultConnection"];
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
